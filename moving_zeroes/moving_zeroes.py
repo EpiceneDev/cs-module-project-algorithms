75daa7aec939c0e444a53d63b7052aeb43aebc16
Sheila Eichenberger <grandpoobah@Sheilas-MacBook-Pro.local>
2020-06-11 18:33:52 -0700

    moving_zeros plan done

--- moving_zeroes/moving_zeroes.py
+++ moving_zeroes/moving_zeroes.py
@@ -1,11 +1,35 @@
 '''
 Input: a List of integers
 Returns: a List of integers
+
+Understand:
+takes an array of integers and moves each non-zero integer 
+    to the left side of the array, 
+    then returns the altered array
+doesn't matter what the order of the altered array
+
+Plan:
+    use 2 pointers to check for 0 one at each end
+    if left pointer sees a zero it swaps with right and 
+    if right pointer sees zero, it moves to next index down
+
 '''
 def moving_zeroes(arr):
-    # Your code here
+    left = 0
+    right = len(arr) - 1
 
-    pass
+    while left <= right:
+        if arr[left] == 0 and arr[right] != 0:
+            arr[left], arr[right] = arr[right], arr[left]
+            left +=1
+            right -=1
+        else: 
+            if arr[left] != 0:
+             left += 1
+            if arr[right] == 0:
+                right -= 1
+
+    return arr
 
 
 if __name__ == '__main__':
